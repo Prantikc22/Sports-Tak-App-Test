3d336809f8939227de8d48c56d4094aa1844e288
prantikc22 <prantikchatterjee22@gmail.com>
2023-01-12 20:20:05 +0100

    Tag removed from test runner.

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -10,8 +10,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         plugin = {"pretty", "html:target/cucumber-report.html", "json:target/cucumber.json", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
         features = {"src/test/resources"},
-        glue = {"stepDefinitions"},
-        tags = "@ChangeLanguage"
+        glue = {"stepDefinitions"}
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
